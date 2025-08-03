ee0c74be9adef1292d6b9ab9d39dc42affd71fdd
mahadi24t <mahadi24t@outlook.com>
2025-08-03 23:56:37 +0600

    initial work

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <img src="" alt="" />
+            <h1>my travel journal</h1>
+        </header>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Header from "../components/Header"
+export default function App(){
+  return(
+    <><h1>Let's goo!</h1></>
+  )
+}
